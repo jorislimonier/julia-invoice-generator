dd0c81694e9c95003bc5eaa7542c3c084897e820
jorislimonier <joris.limonier@hotmail.fr>
2022-06-19 18:17:45 +0200

    Generate empty pdf

---
+++ app.jl
@@ -0,0 +1,37 @@
+module app
+
+using HTTP
+using Parameters
+
+
+@with_kw struct Invoice
+    from::String = "joris"
+    to::String = "helene"
+    date::String = date = "19/06/2022"
+    items::String = """[Dict("name"=> "Shop automation service","quantity"=> 1,"unit_cost"=> 5000)]"""
+end
+
+eval_string(str) = eval(Meta.parse(str::String))
+
+
+"""
+Sends a request and handle response.
+Write to file if status if OK
+"""
+function connect_to_api_and_save_invoice_pdf()
+    url = "https://invoice-generator.com"
+    try
+        response = app.HTTP.post(url)
+        if response.status in [200, 201]
+            open("data/invoices/invoice0.pdf", "w") do f
+                write(f, response.body)
+            end
+        else
+            print("Bad response status $(response.status)")
+        end
+    catch e
+        println("Exception\n", e)
+    end
+end
+
+end
